8f33a63a79054ab564878d9e5cbd1696e4c1c906
sandeepbn2024 <sandeepbn2024@gmail.com>
2024-07-25 23:12:01 -0700

    changes for the state file

--- main.tf
+++ main.tf
@@ -9,12 +9,72 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-  backend "s3" {
-    bucket                  = "STATE-BUCKET-NAME"
-    dynamodb_table          = "DYNAMODB-TABLE-NAME"
-    key                     = "my-terraform-project"
-    region                  = "us-east-1"
-    shared_credentials_file = "~/.aws/credentials"
+ # S3 Bucket for Terraform state
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = var.bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "versioning_enabled" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# S3 Bucket Policy
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-07-25",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "s3:ListBucket",
+        Resource = aws_s3_bucket.terraform_state_bucket.arn,
+        Principal = {
+          AWS = aws_iam_user.terraform_user.arn
+        }
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["s3:GetObject", "s3:PutObject"],
+        Resource = "${aws_s3_bucket.terraform_state_bucket.arn}/*",
+        Principal = {
+          AWS = aws_iam_user.terraform_user.arn
+        }
+      }
+    ]
+  })
+}
+
+# DynamoDB Table for state locking
+resource "aws_dynamodb_table" "state_lock_table" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = var.table_name
   }
 }
 data "aws_iam_policy_document" "assume_role" {
